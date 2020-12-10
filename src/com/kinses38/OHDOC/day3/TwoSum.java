0e4ce5f178ff78b16c6360241a6d0aeb904e0a1b
shaun <shaun.kinsella38@mail.dcu.ie>
2020-12-10 18:56:25 +0000

    Day-3. Finish Romans, Interval and Twosum

---
+++ src/com/kinses38/OHDOC/day3/TwoSum.java
@@ -0,0 +1,28 @@
+package com.kinses38.OHDOC.day3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] numbers, int target){
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < numbers.length; i++){
+            if(map.containsKey(target - numbers[i])){
+                result[0] = map.get(target-numbers[i]);
+                result[1] = i;
+                break;
+            }
+            map.put(numbers[i], i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] test = {1,2,3}; //(0,2)
+        int testTarget = 4;
+
+        System.out.println(Arrays.toString(twoSum(test,testTarget)));
+    }
+}
