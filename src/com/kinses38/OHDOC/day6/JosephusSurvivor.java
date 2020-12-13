36d28fa04ac0273b8c5be9b41b9527f35b9a0d98
shaun <shaun.kinsella38@mail.dcu.ie>
2020-12-13 19:47:27 +0000

    Day 6 josephus survivor

---
+++ src/com/kinses38/OHDOC/day6/JosephusSurvivor.java
@@ -0,0 +1,20 @@
+package com.kinses38.OHDOC.day6;
+
+public class JosephusSurvivor {
+
+    public static int josephusSurvivor(final int n, final int k) {
+        int winner;
+        if(n == 1)
+            return 1;
+        else
+           winner =  (k-1 + josephusSurvivor(n-1,k)) % n + 1;
+        return winner;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 7;
+        int k = 3;
+        System.out.println(josephusSurvivor(n,k));
+    }
+}
