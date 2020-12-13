fa51a6d6c9a22dd0a26d465bacf172375b9e066f
shaun <shaun.kinsella38@mail.dcu.ie>
2020-12-13 19:14:00 +0000

    Day 6 oddint problem

---
+++ src/com/kinses38/OHDOC/day6/FindOdd.java
@@ -0,0 +1,18 @@
+package com.kinses38.OHDOC.day6;
+
+import java.util.HashSet;
+
+public class FindOdd {
+
+    public static int findIt(int[] a) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : a){
+            if(set.contains(i)){
+                set.remove(i);
+            }else{
+                set.add(i);
+            }
+        }
+        return(set.iterator().next());
+    }
+}
